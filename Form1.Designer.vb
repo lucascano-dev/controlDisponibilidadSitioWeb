2c97dd219ee3e4272c59b101d88692831c91020d
Lucas Aaron Cano <lucas_cano@outlook.com>
2024-03-22 12:51:42 -0300

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.stateWeb = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 66)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(185, 31)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Verificar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(12, 28)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(372, 29)
+        Me.TextBox1.TabIndex = 1
+        '
+        'stateWeb
+        '
+        Me.stateWeb.AutoSize = True
+        Me.stateWeb.Font = New System.Drawing.Font("Microsoft YaHei", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.stateWeb.ForeColor = System.Drawing.Color.ForestGreen
+        Me.stateWeb.Location = New System.Drawing.Point(220, 66)
+        Me.stateWeb.Name = "stateWeb"
+        Me.stateWeb.Size = New System.Drawing.Size(20, 26)
+        Me.stateWeb.TabIndex = 2
+        Me.stateWeb.Text = "-"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(12, 3)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(228, 22)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Ingresar URL del sitio web:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.Beige
+        Me.TextBox2.ImeMode = System.Windows.Forms.ImeMode.[On]
+        Me.TextBox2.Location = New System.Drawing.Point(12, 103)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox2.Size = New System.Drawing.Size(373, 103)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(397, 218)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.stateWeb)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Disponibilidad de sitio web"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents stateWeb As System.Windows.Forms.Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Security.Policy
+
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim estado As String
+        stateWeb.Text = ""
+        estado = verificarPagina(TextBox1.Text)
+        If estado Then
+            stateWeb.Text = "Disponible"
+            stateWeb.ForeColor = Color.ForestGreen
+        Else
+            stateWeb.Text = "No disponible"
+            stateWeb.ForeColor = Color.Red
+        End If
+
+    End Sub
+
+    Private Function verificarPagina(ByVal urlPAGINA As String) As Boolean
+        Try
+            Dim request As HttpWebRequest = DirectCast(WebRequest.Create(urlPAGINA), HttpWebRequest)
+            Dim response As HttpWebResponse = DirectCast(request.GetResponse(), HttpWebResponse)
+
+            If response.StatusCode = HttpStatusCode.OK Then
+                TextBox2.Text = ($"Disponible (Código de estado: {response.StatusCode}")
+                Return True
+            Else
+                TextBox2.Text = ($"Disponible (Código de estado: {response.StatusCode}")
+                Return False
+            End If
+        Catch ex As Exception
+            TextBox2.Text = ($"Error al verificar {urlPAGINA}: {ex.Message}")
+            Return False
+        End Try
+    End Function
+End Class
